002a7211b5b9b0fb1d28bb32c2c7df7036e03e9a
tki <tki.git@mm.st>
2023-04-17 15:55:36 +0200

    Fix nft leagues merge

--- src/mappings/leagues.ts
+++ src/mappings/leagues.ts
@@ -7,6 +7,7 @@ import {
   LeftLeague,
   MemberRemoved,
   TransferAdminRole,
+  Leagues as Contract,
 } from '../../generated/Leagues/Leagues'
 
 import { League, Member, Invitation } from '../../generated/schema'
@@ -17,6 +18,7 @@ export function handleNewLeague(event: NewLeague): void {
   let admin = event.params.admin.toHexString()
   let name = event.params.name
   let timestamp = event.block.timestamp
+  let contractAddress = event.address
 
   let league = new League(id)
   let member = new Member(admin)
@@ -87,25 +89,23 @@ export function handleTransferAdminRole(event: TransferAdminRole): void {
   let newAdmin = event.params.newAdmin.toHexString()
   let leagueId = event.params.leagueId.toHexString()
   let league = League.load(leagueId)
-  let newMember = Member.load(newAdmin)
-
   if (league) {
+    let newMember = Member.load(newAdmin)
     if (newMember === null) {
       newMember = new Member(newAdmin)
       newMember.leagues = [league.id]
-      league.membersCount = league.membersCount.plus(BigInt.fromI32(1))
+      newMember.save()
     } else {
       let newMemberLeagues = newMember.leagues
       if (newMemberLeagues) {
         if (newMemberLeagues.indexOf(league.id) === -1) {
           newMemberLeagues.push(leagueId)
           newMember.leagues = newMemberLeagues
-          league.membersCount = league.membersCount.plus(BigInt.fromI32(1))
+          newMember.save()
         }
       }
     }
     league.admin = newAdmin
-    newMember.save()
     league.save()
   } else {
     log.error('Failed to transfer admin role', [])
@@ -138,11 +138,9 @@ function removeMembership(user: Address, id: BigInt): void {
         const removedLeague = memberLeagues.indexOf(leagueId)
         if (removedLeague !== -1) {
           memberLeagues.splice(removedLeague, 1)
-          member.leagues = memberLeagues
-          member.save()
-
           league.membersCount = league.membersCount.minus(BigInt.fromI32(1))
           league.save()
+          member.save()
         }
         if (memberLeagues.length === 0) store.remove('Member', member.id)
       }
